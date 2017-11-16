2a16769f0b49ec1428cb67708489dbb12b3721bf
Andrii_Kazanishen <Andrii_Kazanishen@epam.com>
2017-11-16 14:33:31 +0200

    Add methods and test to KazanishenProj

---
+++ testProj/KazanishenProj/src/main/java/pages/HomePage.java
@@ -0,0 +1,38 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class HomePage extends PerentPage {
+    @FindBy(xpath = ".//span[text()='StudentNew']")
+    WebElement avatar;
+
+    @FindBy(xpath = ".//*[@id='dictionary']/a")
+    private WebElement menuDictionary;
+
+    @FindBy(xpath = ".//a[@href='/dictionary/spares']")
+    private WebElement subMenuSpare;
+
+
+
+    public HomePage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+    public boolean isAvatarPresent() {
+        return actionsWithOurElements.isElementPresent(avatar);
+    }
+
+    public void clickOnMenuDictionary() {
+        actionsWithOurElements.clickOnElement(menuDictionary);
+    }
+
+    public void clickOnSubMenuSpare(){
+        actionsWithOurElements.clickOnElement(subMenuSpare);
+    }
+
+
+
+
+}
---
+++ testProj/KazanishenProj/src/main/java/pages/LoginPage.java
@@ -0,0 +1,60 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage extends PerentPage{
+
+    @FindBy(name = "_username")
+    private WebElement inputLogin;
+
+    @FindBy(name = "_password")
+    private WebElement inputPass;
+
+    @FindBy(tagName = "button")
+    private WebElement buttonLogIn;
+
+    public LoginPage(WebDriver webDriver) {
+        super(webDriver);
+    }
+
+
+    public void openLoginPage(){
+        try{
+            webDriver.get("http://v3.test.itpmgroup.com/login");
+            logger.info("LoginPage was opened");
+        }catch (Exception e){
+            logger.error("Page was not opened");
+            Assert.fail("Page was not opened");
+        }
+    }
+
+    public void enterLoginToInputLogin(String login) {
+//        try{
+//            inputLogin.clear();
+//            inputLogin.sendKeys(login);
+//            logger.info(login + " was inputed into input");
+//        }catch (Exception e){
+//            logger.error("can not work with element");
+//            Assert.fail("can not work with element");
+//        }
+        actionsWithOurElements.enterTextInToInput(inputLogin,login);
+    }
+
+    public void enterPassToInputPass(String pass){
+        actionsWithOurElements.enterTextInToInput(inputPass,pass);
+    }
+
+    public void clickButtonLogIn(){
+        actionsWithOurElements.clickOnElement(buttonLogIn);
+    }
+
+    public void loginInApp(String login, String pass) {
+        openLoginPage();
+        enterLoginToInputLogin(login);
+        enterPassToInputPass(pass);
+        clickButtonLogIn();
+    }
+}
